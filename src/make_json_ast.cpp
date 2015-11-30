1b9b675f0b86194c89c532723cc731000b8c891a
Barret Schloerke <schloerke@gmail.com>
2015-11-30 12:53:40 -0500

    convert the query to json string

---
+++ src/make_json_ast.cpp
@@ -0,0 +1,48 @@
+#include <Rcpp.h>
+
+/**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <AstNode.h>
+#include <GraphQLParser.h>
+#include <c/GraphQLAstToJSON.h>
+// # include "libgraphqlparser/AstNode.h"
+// # include "libgraphqlparser/GraphQLParser.h"
+// # include "libgraphqlparser/c/GraphQLAstToJSON.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+
+using std::cerr;
+using std::endl;
+using std::free;
+
+
+// int main(int argc, char **argv) {
+// [[Rcpp::export]]
+Rcpp::CharacterVector make_json_ast(Rcpp::String queryString) {
+  const char *error;
+
+  auto AST = facebook::graphql::parseString(queryString.get_cstring(), &error);
+
+  if (!AST) {
+    cerr << "Parser failed with error: " << error << endl;
+    free((void *)error);
+    return 1;
+  }
+
+  const char *json = graphql_ast_to_json(
+    (const struct GraphQLAstNode *)AST.get());
+
+  Rcpp::CharacterVector ans(1);
+  ans[0] = json;
+  return ans;
+}
