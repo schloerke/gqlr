e35f8f75a83f02b1e22816f6c1dd6f226948cd87
Barret Schloerke <schloerke@gmail.com>
2016-02-03 14:24:18 -0500

    helper methods

--- R/z_utils.R
+++ R/z_utils.R
@@ -1,9 +1,18 @@
 
 
 
+str_c <- function (..., sep = "", collapse = NULL) {
+  paste(..., sep = sep, collapse = collapse)
+}
+
 str_replace <- function(x, val, replacement) {
   gsub(val, replacement, x)
 }
+
+str_detect <- function(string, pattern, ...) {
+  grepl(pattern, string, ...)
+}
+
 
 
 stop0 <- function(...) {
