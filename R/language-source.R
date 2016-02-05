0ea23096e419818a0db4b0fe05e3c8d001054a00
Barret Schloerke <schloerke@gmail.com>
2016-02-04 20:22:14 -0500

    remove old definition

--- R/language-source.R
+++ R/language-source.R
@@ -1,6 +1,6 @@
 
 
-Source = function(body, name = NULL) {
-  if (identical(name, NULL)) name <- "GraphQL"
-  list(body = body, name = name)
-}
+# Source = function(body, name = NULL) {
+#   if (identical(name, NULL)) name <- "GraphQL"
+#   list(body = body, name = name)
+# }
