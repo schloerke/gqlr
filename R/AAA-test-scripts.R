ea4eb8cae1ad2030b47a31b672a11b8bc804879d
Barret Schloerke <schloerke@gmail.com>
2016-04-11 22:24:34 -0400

    more test scripts

--- R/AAA-test-scripts.R
+++ R/AAA-test-scripts.R
@@ -1,9 +1,10 @@
 
 if (FALSE) {
 
-  load_all(); test_json() %>% r6_from_json() %>% gqlr_str()
+  load_all(); test_json("simple-query") %>% r6_from_json() %>% gqlr_str()
+  load_all(); test_json("kitchen-query") %>% r6_from_json() %>% gqlr_str()
 
-
-
+  load_all(); test_json("simple-schema") %>% r6_from_json() %>% gqlr_str()
+  load_all(); test_json("kitchen-schema") %>% r6_from_json() %>% gqlr_str()
 
 }
