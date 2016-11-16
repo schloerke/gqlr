916faadeea8282a08185868b88b21af241cfa59e
Barret Schloerke <schloerke@gmail.com>
2016-11-16 13:32:37 -0500

    fix a broken pryr unenclose

--- R/AAA-utils.R
+++ R/AAA-utils.R
@@ -22,8 +22,29 @@ str_detect <- function(string, pattern, ...) {
   grepl(pattern, string, ...)
 }
 
-
-
 stop0 <- function(...) {
   stop(paste0(...))
+}
+
+
+
+
+
+pryr_unenclose <- function(f) {
+  stopifnot(is.function(f))
+  env <- environment(f)
+  body <- pryr::modify_lang(body(f), unenclose_a_to_b(env))
+  make_function(formals(f), body, parent.env(env))
+}
+
+unenclose_a_to_b <- function(env, ls_env = ls(envir = env)) {
+  function(x) {
+    if (is.name(x)) {
+      dep_x <- deparse(x)
+      if (dep_x %in% ls_env) {
+        return(get(dep_x, envir = env))
+      }
+    }
+    x
+  }
 }
