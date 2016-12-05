25578a69bd3bb169783761dbd0801e9f5518b4dc
Barret Schloerke <schloerke@gmail.com>
2016-12-04 20:04:08 -0500

    S3 grapql methods

---
+++ R/S3-graphql_str.R
@@ -0,0 +1,15 @@
+
+
+
+graphql_string.Type = function(x, ...) {
+  parse_type <- function(obj) {
+    switch(obj$.kind,
+      "NamedType" = obj$name$value,
+      "ListType" = str_c("[", parse_type(obj$type), "]"),
+      "NonNullType" = str_c(parse_type(obj$type), "!"),
+      stop("type: ", obj$.kind, " not known!")
+    )
+  }
+  # cat("<Name - ", self$value, ">")
+  str_c("`", parse_type(x), "`")
+}
