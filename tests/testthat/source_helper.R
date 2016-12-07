2292297be009056a3856e27bc284f6737440e0f1
Barret Schloerke <schloerke@gmail.com>
2016-12-07 14:21:17 -0500

    Have source helper not populate the global env, and only return an obj

--- tests/testthat/source_helper.R
+++ tests/testthat/source_helper.R
@@ -1,5 +1,13 @@
 
 
 source_kitchen_schema <- function(file) {
-  file.path("kitchen_schema", paste("tks_", file, ".R", sep = ""))
+
+
+  file_path <- file.path("kitchen_schema", paste("tks_", file, ".R", sep = ""))
+
+  env <- new.env()
+  sys.source(file_path, envir = env)
+  obj <- get(paste("tks_", file, sep = ""), envir = env)
+  
+  obj
 }
