8a8e18af61daae2e5c74766d205797d0772fac5e
Barret Schloerke <schloerke@gmail.com>
2016-12-16 17:13:06 -0500

    Validate a lot of the arg requirements

---
+++ R/validation-arguments.R
@@ -0,0 +1,98 @@
+
+#
+# √5.3.1 - Argument Names
+# √5.3.2 - Argument Uniqueness
+# TODO 5.3.3.1 - Compatible Values
+# 5.3.3.2 - Required Non-Null Arguments
+validate_arguments <- function(argument_obj_list, field_def_obj, schema_obj, ...) {
+
+  if (is.null(field_def_obj$arguments)) {
+    stop(
+      "there are no arguments for field: ", graphql_string(field_def_obj$name)
+    )
+  }
+
+  field_arg_map <- field_def_obj$arguments
+  field_arg_map %>%
+    lapply("[[", "name") %>%
+    lapply(graphql_string) %>%
+    unlist() ->
+  names(field_arg_map)
+
+  values_seen <- list()
+
+  for (argument_obj in argument_obj_list) {
+
+    arg_name <- argument_obj$name
+    arg_name_str <- graphql_string(arg_name)
+
+    # 5.3.2 - Argument Uniqueness
+    if (!is.null(values_seen[[arg_name_str]])) {
+      stop("duplicate arguments with same name: ", arg_name_str)
+    }
+
+    arg_value <- argument_obj$value
+    values_seen[[arg_name_str]] <- arg_value
+
+    matching_arg_obj <- field_arg_map[[graphql_string(arg_name)]]
+
+    # 5.3.1 - Argument Names
+    if (is.null(matching_arg_obj)) {
+      stop(
+        "could not find matching arg value with label: ", graphql_string(arg_name),
+        " for field: ", graphql_string(field_def_obj$name)
+      )
+    }
+
+
+
+    # 5.3.3.1 - Compatible Values
+    # If value is not a Variable
+    #   Let argumentName be the Name of argument.
+    #   Let argumentDefinition be the argument definition provided by the parent field or definition named argumentName.
+    #   Let type be the type expected by argumentDefinition.
+    #   The type of literalArgument must be coercible to type.
+    if (inherits(arg_value, "Variable")) {
+      next
+    }
+    # TODO check type
+    # is_coercible_type(arg_value, matching_arg_obj$type)
+
+
+    if (inherits(arg_value, "ObjectValue")) {
+      validate_input_object_field_uniqueness(arg_value, schema_obj, ...)
+    }
+
+  }
+
+
+  # 5.3.3.2 - Required Non-Null Arguments
+  # For each Field or Directive in the document.
+  # Let arguments be the arguments provided by the Field or Directive.
+  # Let argumentDefinitions be the set of argument definitions of that Field or Directive.
+  # For each definition in argumentDefinitions:
+  #   Let type be the expected type of definition.
+  #   If type is Non‐Null:
+  #     Let argumentName be the name of definition.
+  #     Let argument be the argument in arguments named argumentName
+  #     argument must exist.
+  #     Let value be the value of argument.
+  #     value must not be the null literal.
+  for (field_arg in field_arg_map) {
+    if (inherits(field_arg$type, "NonNullType")) {
+      arg_value <- values_seen[[graphql_string(field_arg$name)]]
+      if (
+        is.null(arg_value) ||
+        inherits(arg_value, "NullValue")
+      ) {
+        stop(
+          "null or missing argument not allowed for argument: ", graphql_string(field_arg$name),
+          " for field: ", graphql_string(field_def_obj$name)
+        )
+      }
+    }
+  }
+
+
+  invisible(TRUE)
+}
