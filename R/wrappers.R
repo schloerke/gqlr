f4486527e8af98cc75b1cb6fb1cd23c1239faf5e
Barret Schloerke <schloerke@gmail.com>
2017-01-11 12:07:08 -0500

    Remove unused functions

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,12 +1,3 @@
-#' Test Hello
-#' @export
-test_hello <- function() {
-  queryString <- "query FragmentTyping {\n  profiles(handles: [\"zuck\", \"cocacola\"]) {\n    handle\n    ...userFragment\n    ...pageFragment\n  }\n}\n\nfragment userFragment on User {\n  friends {\n    count\n  }\n}\n\nfragment pageFragment on Page {\n  likers {\n    count\n  }\n}"
-
-  result <- .Call("graphqlr_make_json_ast_fn", PACKAGE = "graphqlr", queryString)
-  result <- from_json(result)
-  return(result)
-}
 
 #' @export
 # load_all(); test_string() %>% make_ast() -> a
@@ -43,21 +34,6 @@ test_json <- function(...) {
   test_string(...) %>%
     eval_json()
 }
-# {
-#   unnamed(truthy: true, falsey: false, nullish: null),
-#   query
-# }
-
-#' Make graphql AST
-#'
-#' @param queryString string to be directly parsed by libgraphqlparser
-#' @export
-make_ast <- function(queryString) {
-  queryString %>%
-    make_json_ast() %>%
-    from_json()
-}
-
 
 from_json <- function(...) {
   rjson::fromJSON(...)
