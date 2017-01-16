f9d73c33efee380eacd9893de840dc20ba3229c7
Barret Schloerke <schloerke@gmail.com>
2017-01-16 15:07:29 -0500

    Allow schema definitions to be generated with resolve functions

--- R/R6z-from-json.R
+++ R/R6z-from-json.R
@@ -1,5 +1,5 @@
 
-r6_from_list <- function(obj, fnList = list(), level = 0, keys = c(), objPos = NULL, verbose = FALSE) {
+r6_from_list <- function(obj, fn_list = NULL, level = 0, keys = c(), objPos = NULL, verbose = FALSE) {
   vcat <- function(...) {
     if (verbose) {
       cat(..., sep = "")
@@ -34,13 +34,43 @@ r6_from_list <- function(obj, fnList = list(), level = 0, keys = c(), objPos = N
         retList[[activeKey]] <- list()
       } else {
         if (identical(class(objVal), "list")) {
+          # going through a list, such as 'Document$defintions' or 'ObjectTypeDefinition$fields'
+
+
+          # str(objVal, max = 2)
+          # print(activeKey)
           # browser()
           # lapply(objVal, r6_from_list, keys = keys, level = level)
           retList[[activeKey]] <- lapply(seq_along(objVal), function(i) {
-            r6_from_list(objVal[[i]], fnList = fnList, keys = keys, level = level, objPos = i, verbose = verbose)
+            if (!is.null(fn_list)) {
+              if (objClass == "Document" && activeKey == "definitions") {
+                obj_name <- objVal[[i]]$name$value
+                fn_list <- fn_list[[obj_name]]
+                description <- fn_list$.description
+                if (!is.null(description)) {
+                  objVal[[i]][["description"]] <- description
+                }
+              } else if (objClass == "ObjectTypeDefinition" && activeKey == "fields") {
+                obj_name <- objVal[[i]]$name$value
+                resolve_fn <- ifnull(fn_list[[obj_name]], default_resolve_key_value)
+                objVal[[i]][[".resolve"]] <- resolve_fn
+                description <- attr(resolve_fn, "description")
+                if (!is.null(description)) {
+                  objVal[[i]][["description"]] <- description
+                }
+                fn_list <- NULL
+              } else {
+                # print(list(class = objClass, activeKey = activeKey))
+                # browser()
+                fn_list <- NULL
+              }
+            }
+
+            r6_from_list(objVal[[i]], fn_list = fn_list, keys = keys, level = level, objPos = i, verbose = verbose)
           })
         } else {
-          retList[[activeKey]] <- r6_from_list(objVal, fnList = fnList, keys = keys, level = level, verbose = verbose)
+          # going into another object, such as "Name" or "Location"
+          retList[[activeKey]] <- r6_from_list(objVal, fn_list = fn_list, keys = keys, level = level, verbose = verbose)
         }
       }
     } else {
@@ -50,4 +80,9 @@ r6_from_list <- function(obj, fnList = list(), level = 0, keys = c(), objPos = N
 
   # browser()
   do.call(r6Obj$new, retList)
+}
+
+
+default_resolve_key_value <- function(obj, args, schema_obj, ...) {
+  return(obj[[key]])
 }
