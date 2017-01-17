239b8c21c437bf71359dfcc41df6361f3a2363e5
Barret Schloerke <schloerke@gmail.com>
2017-01-17 17:06:08 -0500

    r6_from_list now takes in fn_list for methods to work with schema txt

--- R/R6z-from-json.R
+++ R/R6z-from-json.R
@@ -44,25 +44,34 @@ r6_from_list <- function(obj, fn_list = NULL, level = 0, keys = c(), objPos = NU
           retList[[activeKey]] <- lapply(seq_along(objVal), function(i) {
             if (!is.null(fn_list)) {
               if (objClass == "Document" && activeKey == "definitions") {
-                obj_name <- objVal[[i]]$name$value
-                fn_list <- fn_list[[obj_name]]
-                description <- fn_list$.description
-                if (!is.null(description)) {
-                  objVal[[i]][["description"]] <- description
+                fn_list_and_desc <- document_defintion_fn_and_desc(objVal[[i]], fn_list)
+                if (!is.null(fn_list_and_desc$description)) {
+                  objVal[[i]][["description"]] <- fn_list_and_desc$description
                 }
-              } else if (objClass == "ObjectTypeDefinition" && activeKey == "fields") {
-                obj_name <- objVal[[i]]$name$value
-                resolve_fn <- ifnull(fn_list[[obj_name]], default_resolve_key_value)
-                objVal[[i]][[".resolve"]] <- resolve_fn
-                description <- attr(resolve_fn, "description")
-                if (!is.null(description)) {
-                  objVal[[i]][["description"]] <- description
-                }
-                fn_list <- NULL
+
+                fn_list <- fn_list_and_desc$fn_list
+
               } else {
-                # print(list(class = objClass, activeKey = activeKey))
-                # browser()
-                fn_list <- NULL
+                if (objClass == "ObjectTypeDefinition" && activeKey == "fields") {
+                  fn_list_and_desc <- get_resolve_and_description(objVal[[i]], fn_list)
+                  objVal[[i]][[".resolve"]] <- fn_list_and_desc$resolve_fn
+
+                } else {
+
+                  # print(list(class = objClass, activeKey = activeKey))
+                  fn_list_and_desc <- get_resolve_and_description(objVal[[i]], fn_list)
+
+                }
+
+                fn_list <- fn_list_and_desc$fn_list
+
+                # has a description field...
+                if (!is.null(r6Obj$public_fields$.args$description)) {
+                  description <- fn_list_and_desc$description
+                  if (!is.null(description)) {
+                    objVal[[i]][["description"]] <- description
+                  }
+                }
               }
             }
 
@@ -85,4 +94,34 @@ r6_from_list <- function(obj, fn_list = NULL, level = 0, keys = c(), objPos = NU
 
 default_resolve_key_value <- function(obj, args, schema_obj, ...) {
   return(obj[[key]])
+}
+
+
+
+
+document_defintion_fn_and_desc <- function(obj_val_i, fn_list) {
+  obj_name <- obj_val_i$name$value
+  fn_list <- fn_list[[obj_name]]
+  description <- fn_list$.description
+
+  list(fn_list = fn_list, description = description)
+}
+
+
+get_resolve_and_description <- function(obj_val_i, fn_list) {
+  obj_name <- obj_val_i$name$value
+
+  resolve_fn <- fn_list[[obj_name]]
+  if (is.character(resolve_fn)) {
+    description <- resolve_fn
+    resolve_fn <- NULL
+
+  } else {
+    description <- attr(resolve_fn, "description")
+
+  }
+
+  resolve_fn <- ifnull(resolve_fn, default_resolve_key_value)
+
+  list(fn_list = NULL, description = description, resolve_fn = resolve_fn)
 }
