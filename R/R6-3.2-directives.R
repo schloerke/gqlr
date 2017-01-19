a8460f661a2e7810421574e67ebdf0eca01c2751
Barret Schloerke <schloerke@gmail.com>
2017-01-18 22:32:43 -0500

    Update directive definitions to use graphql2obj and fn_list

--- R/R6-3.2-directives.R
+++ R/R6-3.2-directives.R
@@ -1,4 +1,5 @@
 #' @include R6--definition.R
+#' @include graphql_json.R
 
 
 DirectiveLocationNames <- (function() {
@@ -31,39 +32,70 @@ DirectiveLocationNames <- (function() {
 })()
 
 
-SkipDirective <- DirectiveDefinition$new(
-  name = Name$new(value = "skip"),
-  arguments = list(
-    InputValueDefinition$new(
-      name = Name$new(value = "if"),
-      type = NonNullType(type = NamedType$new(name = Name$new(value = "Boolean")))
+"
+directive @skip(if: Boolean!)
+  on FIELD
+   | FRAGMENT_SPREAD
+   | INLINE_FRAGMENT
+directive @include(if: Boolean!)
+  on FIELD
+   | FRAGMENT_SPREAD
+   | INLINE_FRAGMENT
+" %>%
+  graphql2obj(fn_list = list(
+    skip = list(
+      description = "awesome!",
+      resolve = function(if_val) {
+        !isTRUE(if_val)
+      }
+    ),
+    include = list(
+      resolve = function(if_val) {
+        isTRUE(if_val)
+      }
     )
-  ),
-  locations = list(
-    DirectiveLocationNames$FIELD,
-    DirectiveLocationNames$FRAGMENT_SPREAD,
-    DirectiveLocationNames$INLINE_FRAGMENT
-  ),
-  .resolve = function(if_val) {
-    !isTRUE(if_val)
-  }
-)
+  )) ->
+directive_document
+
+# ' @export
+SkipDirective <- directive_document$definitions[[1]]
+# ' @export
+IncludeDirective <- directive_document$definitions[[2]]
 
 
-IncludeDirective <- DirectiveDefinition$new(
-  name = Name$new(value = "include"),
-  arguments = list(
-    InputValueDefinition$new(
-      name = Name$new(value = "if"),
-      type = NonNullType$new(type = NamedType$new(name = Name$new(value = "Boolean")))
-    )
-  ),
-  locations = list(
-    DirectiveLocationNames$FIELD,
-    DirectiveLocationNames$FRAGMENT_SPREAD,
-    DirectiveLocationNames$INLINE_FRAGMENT
-  ),
-  .resolve = function(if_val) {
-    isTRUE(if_val)
-  }
-)
+# SkipDirective <- DirectiveDefinition$new(
+#   name = Name$new(value = "skip"),
+#   arguments = list(
+#     InputValueDefinition$new(
+#       name = Name$new(value = "if"),
+#       type = NonNullType(type = NamedType$new(name = Name$new(value = "Boolean")))
+#     )
+#   ),
+#   locations = list(
+#     DirectiveLocationNames$FIELD,
+#     DirectiveLocationNames$FRAGMENT_SPREAD,
+#     DirectiveLocationNames$INLINE_FRAGMENT
+#   ),
+#   .resolve = function(if_val) {
+#     !isTRUE(if_val)
+#   }
+# )
+#
+#
+# IncludeDirective <- DirectiveDefinition$new(
+#   name = Name$new(value = "include"),
+#   arguments = list(
+#     InputValueDefinition$new(
+#       name = Name$new(value = "if"),
+#       type = NonNullType$new(type = NamedType$new(name = Name$new(value = "Boolean")))
+#     )
+#   ),
+#   locations = list(
+#     DirectiveLocationNames$FIELD,
+#     DirectiveLocationNames$FRAGMENT_SPREAD,
+#     DirectiveLocationNames$INLINE_FRAGMENT
+#   ),
+#   .resolve = function(if_val) {
+#     isTRUE(if_val)
+#   }
+# )
