c31250afecf02552325980a398a62c7573c440be
Barret Schloerke <schloerke@gmail.com>
2017-04-29 18:39:11 -0400

    Test mutation works

---
+++ tests/testthat/test-z-execute-mutation-simple.R
@@ -0,0 +1,71 @@
+# load_all(); testthat::test_file(file.path("tests", "testthat", "test-z-execute-mutation-simple.R"))
+
+
+context("execute-simple")
+
+source("validate_helper.R")
+
+test_that("small counter", {
+
+  count_total <- 0
+  "
+  type Counter {
+    value: Int
+  }
+  type AddCounter {
+    value: Int
+  }
+  schema {
+    query: Counter
+    mutation: AddCounter
+  }
+  " %>%
+    graphql2obj(
+      Counter = list(
+        .resolve = function(...) {
+          list(value = count_total)
+        }
+      ),
+      AddCounter = list(
+        .resolve = function(...) {
+          list(
+            value = function(z1, z2, schema_obj) {
+              count_total <<- count_total + 1
+              count_total
+            }
+          )
+        }
+      )
+    ) ->
+  schema_doc
+
+
+  do_query <- function(i) {
+    ans <- execute_request(
+      "{
+        value
+      }",
+      schema_doc
+    )
+    expect_true(ans$error_list$has_no_errors())
+    expect_equal(ans$data$value, i)
+  }
+
+  do_mutation <- function(i) {
+    ans <- execute_request(
+      "mutation add_value{
+        value
+      }",
+      schema_doc
+    )
+    expect_true(ans$error_list$has_no_errors())
+    expect_equal(ans$data$value, i)
+  }
+
+  for (i in 0:10) {
+    do_query(i)
+    do_mutation(i + 1)
+  }
+
+
+})
