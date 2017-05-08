8c1535cb6ca6ef9df3b319df02be72d4e81a6380
Barret Schloerke <schloerke@gmail.com>
2017-05-07 21:46:13 -0400

    remove lintr from travis

--- tests/testthat/test-zzzzz-lintr.R
+++ tests/testthat/test-zzzzz-lintr.R
@@ -1,20 +1,17 @@
 
 # https://github.com/jimhester/lintr
 if (requireNamespace("lintr", quietly = TRUE)) {
-  if (Sys.getenv("LINTR") != "") {
+  context("lints")
+  test_that("Package Style", {
 
-    context("lints")
-    test_that("Package Style", {
+    (function() {
+      Sys.setlocale(locale = "C")
+      on.exit({
+          Sys.setlocale(locale = "en_US.UTF-8")
+      })
 
-      (function() {
-        Sys.setlocale(locale = "C")
-        on.exit({
-            Sys.setlocale(locale = "en_US.UTF-8")
-        })
+      lintr::expect_lint_free(cache = TRUE)
+    })()
 
-        lintr::expect_lint_free(cache = TRUE)
-      })()
-
-    })
-  }
+  })
 }
