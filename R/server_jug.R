468d7dba62a09ae6677e57bbdae515d72189e744
Barret Schloerke <schloerke@gmail.com>
2019-11-26 14:46:57 -0500

    Change server to plumber from jug

--- R/server_jug.R
+++ R/server_jug.R
@@ -5,7 +5,7 @@
 
 #' Run basic GraphQL server
 #'
-#' Run a basic GraphQL server with the jug package.  This server is provided to show basic interaction with GraphQL.  The server will run until the function execution is canceled.
+#' Run a basic GraphQL server using plumber.  This server is provided to show basic interaction with GraphQL.  The server will run until the function execution is canceled.
 #'
 #' \code{server()} implements the basic necessities described in \url{http://graphql.org/learn/serving-over-http/}.  There are three routes implemented:
 #'
@@ -32,174 +32,203 @@
 #'   # GET Schema definition
 #'   curl '127.0.0.1:8000/'
 #'
+#'   ## POST for R2-D2 and his friends' names
+#'   # defaults to parse as JSON
+#'   curl --data '{"query":"{hero{name, friends { name }}}", "pretty": true}' '127.0.0.1:8000/graphql'
+#'   # send json header
+#'   curl --data '{"query":"{hero{name, friends { name }}}"}' '127.0.0.1:8000/graphql' --header "Content-Type:application/json"
+#'   # send graphql header
+#'   curl --data '{hero{name, friends { name }}}' '127.0.0.1:8000/graphql' --header "Content-Type:application/graphql"
+#'   # use variables
+#'   curl --data '{"query":"query Droid($someId: String!) {droid(id: $someId){name, friends { name }}}", "variables": {"someId": "2001"}}' '127.0.0.1:8000/graphql'
+#'
 #'   # GET R2-D2 and his friends' names
 #'   curl '127.0.0.1:8000/graphql?query=%7Bhero%7Bname%2Cfriends%7Bname%7D%7D%7D&pretty=TRUE'
+#'   # ... using a variable
+#'   curl '127.0.0.1:8000/graphql?query=query%20Droid(%24someId%3A%20String!)%20%7Bdroid(id%3A%20%24someId)%7Bname%2C%20friends%20%7B%20name%20%7D%7D%7D&variables=%7B%22someId%22%3A%222001%22%7D'
 #'
-#'   # POST for R2-D2 and his friends' names
-#'   curl --data '{"query":"{hero{name}}"}' '127.0.0.1:8000/graphql' # defaults to parse as JSON
-#'   curl --data '{"query":"{hero{name}}"}' '127.0.0.1:8000/graphql' --header "Content-Type:application/json"
-#'   curl --data '{hero{name}}' '127.0.0.1:8000/graphql' --header "Content-Type:application/graphql"
 #' }
 #'
 #' @param schema Schema object to use execute requests
-#' @param port web port to serve the server from.  Set port to \code{NULL} to not run the jug server and return it.
+#' @param port web port to serve the server from.  Set port to \code{NULL} to not run the plumber server and return it.
 #' @param log boolean that determines if server logging is done.  Defaults to TRUE
 # nocov start
 #' @param initial_value default value to use in \code{\link{execute_request}()}
+#' @export
 server <- function(schema, port = 8000L, log = TRUE, initial_value = NULL) {
 
-  if (!requireNamespace("jug")) {
-    stop("jug must be installed.  install.packages('jug')")
+  if (!requireNamespace("plumber")) {
+    stop("plumber must be installed.  `install.packages('plumber')`")
   }
 
   # Create a new router
-  jug_router <- jug::jug()
+  pr <- plumber::plumber$new()
   if (isTRUE(log)) {
-    jug_router <- jug_router %>%
-      jug::get(path = NULL, function(req, res, err) {
-        print(paste0(
-          date(), " - ",
-          "GET", " ",
-          paste(
-            c(
-              req$path,
-              if (!is.null(req$params$query))
-                paste0("query: '", req$params$query, "'"),
-              if (!is.null(req$params$variables))
-                paste0("variables: '", req$params$variables, "'"),
-              if (!is.null(req$params$operationName))
-                paste0("operationName: '", req$params$operationName, "'")
-            ),
-            sep = " - "
-          )
-        ))
+    log_var <- function(val, name) {
+      if (is.null(val)) return(NULL)
+      if (is.list(val)) {
+        val <- to_json(val, pretty = FALSE)
+      }
+      paste0(name, ": '", val, "'")
+    }
+    pr$registerHook("postroute", function(data, req, res) {
+      vars <- ifnull(req[["_gqlr"]], list())
+      cat(paste0(
+        date(), " - ",
+        req$REQUEST_METHOD, " ",
+        paste(
+          c(
+            req$PATH_INFO,
+            log_var(vars$query, "query"),
+            log_var(vars$variables, "variables"),
+            log_var(vars$operation_name, "operationName")
+          ),
+          collapse = " - "
+        ),
+        "\n"
+      ))
 
-        NULL
-      })
+      NULL
+    })
   }
 
 
-  server_execute_query <- function(res, query, variables, operation_name, pretty) {
+  server_execute_query <- function(req, res, query, variables, operation_name, pretty) {
+
+    pretty <- isTRUE(as.logical(ifnull(pretty, FALSE)))
 
     if (!is.character(query) || is.null(query)) {
-      ans <- list(
-        data = NULL,
-        errors = list(
-          list(
-            message = "server(): 'query' parameter must be supplied as a GraphQL string"
-          )
-        )
-      )
-      res$status <- 500L
-      res$json(ans)
-      return()
+      stop("'query' parameter must be supplied as a GraphQL string")
     }
+
+    req[["_gqlr"]] <- list(
+      query = query,
+      variables = variables,
+      operation_name = operation_name,
+      pretty = pretty
+    )
 
     ans <- execute_request(
       query,
       schema,
-      variables = variables,
+      variables = ifnull(variables, list()),
       operation_name = operation_name,
       initial_value = initial_value
     )
 
     ret <- ans$as_json(pretty = pretty)
 
-    res$content_type("application/json")
     if (!is.null(ans$errors)) {
       res$status <- 500L
     }
-    res$body <- ret
 
-    return()
+    ret
   }
 
+  set_res_json_serializer <- function(res) {
+    res$serializer <- function(val, req, res, errorHandler) {
+      tryCatch({
+        res$setHeader("Content-Type", "application/json")
+        res$body <- to_json(val)
+        return(res$toResponse())
+      }, error = function(e) {
+        errorHandler(req, res, e)
+      })
+    }
 
+    invisible(res)
+  }
 
-  server <- jug_router %>%
+  pr$set404Handler(function(req, res, err) {
+    res$status <- 404
+    set_res_json_serializer(res)
 
-    jug::get(path = "/", function(req, res, err) {
-      ans <- format(schema$get_schema())
-      res$text(ans)
+    list(
+      data = NULL,
+      errors = list(
+        list(
+          message = str_c(
+            "server(): route '", req$PATH_INFO, "' not served.",
+            "  gqlr::server() only understands '/' and '/graphql'" # nolint
+          )
+        )
+      )
+    )
 
-      # makes it "true" GraphQL, but it can't be viewed in a browser
-      # nolint start
-      ## res$content_type("application/graphql")
-      ## res$body <- ans
-      ## NULL
-      # nolint end
+  })
+  pr$setErrorHandler(function(req, res, err) {
 
-    }) %>%
+    set_res_json_serializer(res)
+    res$status <- 500L
+    errs_string <- paste("server(): ", paste(err$message, collapse = "\n"))
+    cat("ERROR:\n", errs_string, "\n")
 
-    jug::post(path = "/graphql", function(req, res, err) { # nolint
+    list(
+      data = NULL,
+      errors = list(list(message = errs_string))
+    )
+  })
 
-      if (identical(req$headers$content_type, "application/json")) {
-        query <- req$params$query
-        variables <- req$params$variables # can be null
-        operation_name <- req$params$operationName # can be null
-        pretty <- isTRUE(as.logical(req$params$pretty)) # can be null
-      } else if (identical(req$headers$content_type, "application/graphql")) {
-        query <- req$body
+  pr$handle(
+    "GET", "/",
+    ## makes it "true" GraphQL, but it can't be viewed in a browser
+    # serializer = plumber::serializer_content_type("application/graphql"),
+    serializer = plumber::serializer_content_type("text/plain"),
+    function() {
+      format(schema$get_schema())
+    }
+  )
+
+  pr$handle(
+    "POST", "/graphql",
+    # must preempt on postBody as graphql code triggers at json, but fails to parse.
+    preempt = "postBody",
+    serializer = plumber::serializer_content_type("application/json"),
+    function(req, res) {
+
+      body <- paste0(req$rook.input$read_lines(), collapse = "\n")
+
+      if (identical(req$HEADERS[['content-type']], "application/graphql")) {
+        query <- body
         variables <- list()
         operation_name <- NULL
         pretty <- FALSE
+
       } else {
-        body <- jsonlite::fromJSON(req$body)
+        if (!jsonlite::validate(body)) {
+          stop("non-json body provided to POST /graphql")
+        }
+        body <- jsonlite::fromJSON(body)
         query <- body$query
-        variables <- ifnull(body$variables, list())
+        variables <- body$variables
         operation_name <- body$operationName
-        pretty <- isTRUE(as.logical(ifnull(body$pretty, FALSE)))
+        pretty <- body$pretty
       }
 
-      server_execute_query(res, query, variables, operation_name, pretty)
-    }) %>%
+      server_execute_query(req, res, query, variables, operation_name, pretty)
+    }
+  )
 
-    jug::get(path = "/graphql", function(req, res, err) { # nolint
-      query <- req$params$query
-      if (is.null(req$params$variables)) {
-        variables <- NULL
-      } else {
-        variables <- jsonlite::fromJSON(req$params$variables)
+  pr$handle(
+    "GET", "/graphql",
+    serializer = plumber::serializer_content_type("application/json"),
+    function(req, res) {
+      query <- req$args$query
+      variables <- req$args$variables
+      if (!is.null(req$args$variables)) {
+        variables <- jsonlite::fromJSON(req$args$variables)
       }
-      operation_name <- req$params$operationName # can be null
-      pretty <- isTRUE(as.logical(req$params$pretty)) # can be null
+      operation_name <- req$args$operationName # can be null
+      pretty <- req$args$pretty # can be null
 
-      server_execute_query(res, query, variables, operation_name, pretty)
-    }) %>%
+      server_execute_query(req, res, query, variables, operation_name, pretty)
+    }
+  )
 
-    # jug::simple_error_handler_json() %>%
-    jug::use(path = NULL, function(req, res, err) {
-      res$content_type("application/json")
-      if (err$occurred) {
-        res$status <- 500L
-        errs_string <- paste("server(): ", paste(err$errors, collapse = "\n"))
-        if (getOption("jug.verbose"))
-          cat("ERROR:\n", errs_string, "\n")
-        res$body <- to_json(list(
-          data = NULL,
-          errors = list(list(message = errs_string))
-        ))
-      } else {
-        res$status <- 404L
-        res$body <- to_json(list(
-          data = NULL,
-          errors = list(
-            list(
-              message = str_c(
-                "server(): route '", req$path, "' not served.",
-                "  gqlr::server() only understands '/' and '/graphql'" # nolint
-              )
-            )
-          )
-        ))
-      }
-      NULL
-    })
-
-  if (is.null(port)) {
-    server %>% jug::serve_it(port = port)
+  if (!is.null(port)) {
+    pr$run(port = port)
   }
 
-  invisible(server)
+  pr
 }
 # nocov end
